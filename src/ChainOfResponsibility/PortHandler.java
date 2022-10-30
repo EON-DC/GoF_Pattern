4e5eb233624cba2ff61627b1eb076a2d26567edc
EON-DC <mun0392@gmail.com>
2022-10-30 18:36:11 +0900

    Gof pattern

--- src/ChainOfResponsibility/PortHandler.java
+++ src/ChainOfResponsibility/PortHandler.java
@@ -4,7 +4,7 @@ public class PortHandler extends Handler{
 
     @Override
     protected void process(String url) {
-        int dotIndex = url.lasgindexOf(":");
+        int dotIndex = url.lastIndexOf(":");
         if (dotIndex == -1) {
             System.out.println("NO Port");
         } else {
